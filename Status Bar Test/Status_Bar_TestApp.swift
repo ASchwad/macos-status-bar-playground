b9f862ebaac535e8ef680bcdb076f61782d3c906
ASchwad <a.schoenenwald@gmx.de>
2024-11-01 12:59:50 +0100

    Initial Commit

---
+++ Status Bar Test/Status_Bar_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Status_Bar_TestApp.swift
+//  Status Bar Test
+//
+//  Created by Alexander Schoenenwald on 01.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Status_Bar_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Status Bar TestUITests/Status_Bar_TestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Status_Bar_TestUITestsLaunchTests.swift
+//  Status Bar TestUITests
+//
+//  Created by Alexander Schoenenwald on 01.11.24.
+//
+
+import XCTest
+
+final class Status_Bar_TestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
